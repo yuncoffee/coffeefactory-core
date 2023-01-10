960c538e29da565bb67f62f4e7187a8a13685ea1
yuncoffee <dong072815@gmail.com>
2023-01-11 00:18:37 +0900

    feat : color 생성기 작성 완료 #2

--- src/data/type.ts
+++ src/data/type.ts
@@ -1,1 +1,26 @@
 export const SIZE_LIST = ["xxl", "xl", "lg", "mid", "sm", "xs", "xxs"] as const
+
+export const COLOR_LIST = [
+    "scalet",
+    "red",
+    "orange",
+    "yellow",
+    "lightgreen",
+    "green",
+    "teal",
+    "blue",
+    "deepblue",
+    "navy",
+    "lavendar",
+    "violet",
+    "purple",
+    "pink",
+] as const
+
+export const COLOR_TYPE_LIST = [
+    "lightness",
+    "light",
+    "base",
+    "dark",
+    "darkness",
+]
